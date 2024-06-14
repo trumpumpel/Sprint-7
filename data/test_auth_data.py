40d84b57f056831a0424307675a697f2fd6b02a6
trumpumpel <57648507+trumpumpel@users.noreply.github.com>
2024-06-14 17:38:30 +0700

    Перенёс generate_random_string в helpers.py

--- data/test_auth_data.py
+++ data/test_auth_data.py
@@ -1,21 +1,10 @@
-import string
-import random
+from helpers import login, password, first_name
 
 
 class TestAuthData:
     lg = "murfl"
     ps = '234634'
     fn = "burfl"
-
-    @staticmethod
-    def generate_random_string(length):
-        letters = string.ascii_lowercase
-        random_string = ''.join(random.choice(letters) for i in range(length))
-        return random_string
-
-    login = generate_random_string(10)
-    password = generate_random_string(10)
-    first_name = generate_random_string(10)
 
     data = {
         "firstName": "Naruto",
